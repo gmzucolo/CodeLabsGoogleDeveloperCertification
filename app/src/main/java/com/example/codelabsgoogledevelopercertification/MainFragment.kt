348a984f3d89b457595d9f2e595c04e6388858be
gmzucolo <gustavo.zucolo@hotmail.com>
2022-04-19 10:54:21 -0300

    nav_graph implemented

---
+++ app/src/main/java/com/example/codelabsgoogledevelopercertification/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.example.codelabsgoogledevelopercertification
+
+import android.os.Bundle
+import android.view.View
+import com.example.codelabsgoogledevelopercertification.databinding.FragmentMainBinding
+
+class MainFragment : androidx.fragment.app.Fragment(R.layout.fragment_main) {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+    }
+}
